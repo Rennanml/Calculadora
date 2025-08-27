c33cc46262270d5ca5d1122cdb1fcee322acce66
Rennan Marcile Lazarini <rennanml112@gmail.com>
2025-08-27 18:29:47 -0300

    feat: add evaluate logic

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039005/calculadora/MainActivity.kt
@@ -0,0 +1,85 @@
+package br.edu.ifsp.scl.ads.prdm.sc3039005.calculadora
+
+import android.os.Bundle
+import br.edu.ifsp.scl.ads.prdm.sc3039005.calculadora.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import net.objecthunter.exp4j.ExpressionBuilder
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.numeroZero.setOnClickListener { acrescentarExpressao("0", true) }
+        binding.numeroUm.setOnClickListener { acrescentarExpressao("1", true) }
+        binding.numeroDois.setOnClickListener { acrescentarExpressao("2", true) }
+        binding.numeroTres.setOnClickListener { acrescentarExpressao("3", true) }
+        binding.numeroQuatro.setOnClickListener { acrescentarExpressao("4", true) }
+        binding.numeroCinco.setOnClickListener { acrescentarExpressao("5", true) }
+        binding.numeroSeis.setOnClickListener { acrescentarExpressao("6", true) }
+        binding.numeroSete.setOnClickListener { acrescentarExpressao("7", true) }
+        binding.numeroOito.setOnClickListener { acrescentarExpressao("8", true) }
+        binding.numeroNove.setOnClickListener { acrescentarExpressao("9", true) }
+        binding.ponto.setOnClickListener { acrescentarExpressao(".", true) }
+
+        binding.adicao.setOnClickListener { acrescentarExpressao("+", false) }
+        binding.subtracao.setOnClickListener { acrescentarExpressao("-", false) }
+        binding.multiplicacao.setOnClickListener { acrescentarExpressao("*", false) }
+        binding.divisao.setOnClickListener { acrescentarExpressao("/", false) }
+
+        binding.clear.setOnClickListener {
+            binding.expressao.text = ""
+            binding.resultado.text = ""
+        }
+
+        binding.apagar.setOnClickListener {
+            val string = binding.expressao.text.toString()
+            if (string.isNotBlank()) {
+                binding.expressao.text = string.substring(0, string.length - 1)
+            }
+            binding.resultado.text = ""
+        }
+
+        binding.igual.setOnClickListener {
+            try {
+                val expressao = ExpressionBuilder(binding.expressao.text.toString()).build()
+                val resultado = expressao.evaluate().toString()
+                val doubleResultado = resultado.toDouble()
+
+                if (resultado == doubleResultado.toString()) {
+                    binding.resultado.text = doubleResultado.toString()
+                }
+                else {
+                    binding.resultado.text = resultado
+                }
+            }
+            catch (e: Exception) {
+                binding.resultado.text = "Erro"
+                binding.expressao.text = ""
+            }
+        }
+    }
+
+    fun acrescentarExpressao(string: String, limpar_dados: Boolean) {
+        if (binding.resultado.text.isNotEmpty()) {
+            binding.expressao.text = ""
+        }
+
+        if (limpar_dados) {
+            binding.resultado.text = ""
+            binding.expressao.append(string)
+        }
+
+        else {
+            binding.expressao.append(binding.resultado.text)
+            binding.expressao.append(string)
+            binding.resultado.text = ""
+        }
+    }
+}
